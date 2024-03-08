d77f8035128be8da02e5b26977b175a5d3d92e97
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-08 11:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const currentURL = window.location.href;
+const currentLength = currentURL.length;
+
+// const urlLenght = prompt('The length of the URL is:', currentURL);
+alert(currentLength);
